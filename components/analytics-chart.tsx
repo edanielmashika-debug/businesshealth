7fb03559e50fb6d0b483457d9fb5af8cf5a4c777
Emmanuel Daniel <edaniel.mashika@gmail.com>
2026-06-03 02:23:20 -0700

    analii

--- components/analytics-chart.tsx
+++ components/analytics-chart.tsx
@@ -1,58 +1,131 @@
-"use client"
- import{
-    PieChart,
-    Pie,
-    Cell,
-    Tooltip,
-    ResponsiveContainer,
- } from "recharts";
+"use client";
 
- import { useDashboardStats } from "@/hooks/use-dashboard-stats";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 
- export default function AnalyticsChart() {
-    const{
-        income, 
-        expense,
-    } = useDashboardStats();
+import {
+  useSalesStore,
+} from "@/store/sales-store";
 
-    const data =  [
-        {
-            name: "Income",
-            value: income,
-        },
+export default function AnalyticsChart() {
 
-        {
-            name: "Expenses",
-            value: expense,
-        },
-    ];
+  const sales =
+    useSalesStore(
+      (state) =>
+        state.sales
+    );
 
-    return(
-        <div className="border rounded-2x1 p-5">
-            <h2 className="text-x1 font-bold mb-4 text-black">
-                Analytics
-            </h2>
-            <div className="h-[300px]">
-                <ResponsiveContainer
-                  width="100%"
-                  height="100%"
-                  >
-                    <PieChart>
-                        <Pie
-                          data={data}
-                          dataKey="value"
-                          outerRadius={100}
-                          label
-                          >
-                            <Cell fill="#57f18f"/>
-                            <Cell fill="#f55555"/>
-                          </Pie>
+  const groupedSales:
+    Record<
+      string,
+      number
+    > = {};
 
-                          <Tooltip/>
-                    </PieChart>
-                  </ResponsiveContainer>
+  sales.forEach(
+    (sale) => {
 
-            </div>
-        </div>
-    )
- }
+      const date =
+        new Date(
+          sale.createdAt
+        ).toLocaleDateString();
+
+      if (
+        groupedSales[date]
+      ) {
+
+        groupedSales[
+          date
+        ] += sale.total;
+
+      } else {
+
+        groupedSales[
+          date
+        ] = sale.total;
+      }
+    }
+  );
+
+  const data =
+    Object.entries(
+      groupedSales
+    ).map(
+      ([date, revenue]) => ({
+        date,
+        revenue,
+      })
+    );
+
+  return (
+
+    <div className="h-[350px]">
+
+      <ResponsiveContainer
+        width="100%"
+        height="100%"
+      >
+
+        <AreaChart
+          data={data}
+        >
+
+          <defs>
+
+            <linearGradient
+              id="colorRevenue"
+              x1="0"
+              y1="0"
+              x2="0"
+              y2="1"
+            >
+
+              <stop
+                offset="5%"
+                stopColor="#3b82f6"
+                stopOpacity={0.4}
+              />
+
+              <stop
+                offset="95%"
+                stopColor="#3b82f6"
+                stopOpacity={0}
+              />
+            </linearGradient>
+          </defs>
+
+          <CartesianGrid
+            strokeDasharray="3 3"
+            stroke="#e5e7eb"
+          />
+
+          <XAxis
+            dataKey="date"
+            stroke="#94a3b8"
+          />
+
+          <YAxis
+            stroke="#94a3b8"
+          />
+
+          <Tooltip />
+
+          <Area
+            type="monotone"
+            dataKey="revenue"
+            stroke="#2563eb"
+            strokeWidth={4}
+            fillOpacity={1}
+            fill="url(#colorRevenue)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
